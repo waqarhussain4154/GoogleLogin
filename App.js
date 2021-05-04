4fed8b86762b110268ba0586d0b882518aa4c04f
waqarhussain4154 <waqarhussain4154@gmail.com>
2021-05-04 15:48:47 +0500

    google login

---
+++ App.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: null,
+      gettingLoginStatus: true,
+    };
+  }
+
+  componentDidMount() {
+    //initial configuration
+    GoogleSignin.configure({
+      //It is mandatory to call this method before attempting to call signIn()
+      scopes: ['openid','profile','email'],
+      // Repleace with your webClientId generated from Firebase console
+      webClientId: '22610120978-26ves0jt2r4atalc1ra6a2h7d6pukv0k.apps.googleusercontent.com',
+    });
+    //Check if user is already signed in
+    this._isSignedIn();
+  }
+
+  _isSignedIn = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    if (isSignedIn) {
+      alert('User is already signed in');
+      //Get the User details as user is already signed in
+      this._getCurrentUserInfo();
+    } else {
+      //alert("Please Login");
+      console.log('Please Login');
+    }
+    this.setState({ gettingLoginStatus: false });
+  };
+
+  _getCurrentUserInfo = async () => {
+    try {
+      const userInfo = await GoogleSignin.signInSilently();
+      console.log('User Info --> ', userInfo);
+      this.setState({ userInfo: userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+        alert('User has not signed in yet');
+        console.log('User has not signed in yet');
+      } else {
+        alert("Something went wrong. Unable to get user's info");
+        console.log("Something went wrong. Unable to get user's info");
+      }
+    }
+  };
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+        console.warn(error)
+      }
+    }
+  };
+
+  render() {
+    //returning Loader untill we check for the already signed in user
+    if (this.state.gettingLoginStatus) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
+    } else {
+      if (this.state.userInfo != null) {
+        //Showing the User detail
+        return (
+          <View style={styles.container}>
+            <Image
+              source={{ uri: this.state.userInfo.user.photo }}
+              style={styles.imageStyle}
+            />
+            <Text style={styles.text}>
+              Name: {this.state.userInfo.user.name}{' '}
+            </Text>
+            <Text style={styles.text}>
+              Email: {this.state.userInfo.user.email}
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={this._signOut}>
+              <Text>Logout</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      } else {
+        //For login showing the Signin button
+        return (
+          <View style={styles.container}>
+            <GoogleSigninButton
+              style={{ width: 312, height: 48 }}
+              size={GoogleSigninButton.Size.Wide}
+              color={GoogleSigninButton.Color.Light}
+              onPress={this._signIn}
+            />
+          </View>
+        );
+      }
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    width: 200,
+    height: 300,
+    resizeMode: 'contain',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 30,
+  },
+});
